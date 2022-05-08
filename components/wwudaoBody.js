a0431f66657ad803f2587a71003ce125f4adf164
aaronleekers <62626022+aaronleekers@users.noreply.github.com>
2022-05-07 21:13:53 -0700

    Add files via upload

---
+++ components/wwudaoBody.js
@@ -0,0 +1,33 @@
+
+import '../components/wwudaoBody.css'
+
+
+const WwudaoBody = ({}) => {
+
+
+    return (
+        <div className='bodyContainer'>     
+            <div className='textContainer'>
+                <h1 className='whatIs'>What is the wwuDAO?</h1>
+                <div>
+                    <div className='bodyText'>
+                        <p>
+                            wwuDAO is an organization that is owned and operated by students of Walla Walla University, for the purpose of empowering students to network and collaborate on cutting edge technology. The DAO in wwuDAO stands for decentralized autonomous organization, which is a fancy way of a digital community.
+                        </p>
+                        <p>
+                            Shifting needs will determine the DAO's purpose and place on campus. Thanks to blockchain technology, the DAO can maintain a near perfect record of members, published content, votes, etc. It has the ability to serve as a snapshot in time, giving the officers who work on it important experience into the tech and Web3 industries.
+                        </p>
+                        <p>
+                            The DAO will operate under execution of the officers, updating it through proposals to all members, who will decide the direction of the DAO through the DAO native token, WLA, which is airdropped to members & officers at the beginning of each membership period. At the end of every membership period, these tokens will be burned and redistributed to ensure everyone has a fair vote.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className='imageContainer'>
+            <img className='fivePoints' src='https://bafkreig7i3t66ymnznw24vsopsyxxj6yodd77okbt3rsmro52mf7bxdwfu.ipfs.nftstorage.link/' alt=''/>
+            </div>
+        </div>
+    )
+}
+
+export default WwudaoBody
